b82a46c61ab99a99649167b2ec0a9ac24bca429d
Mizofabio <yokomizo.fabio@gmail.com>
2023-02-24 15:51:15 -0300

    Variaveis e constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+
+        double salarioMinimo = 2500.79; // valor 2.500,79
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // cast
+
+        // variavel e constantes
+
+        int numero = 5;
+
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;  // constante(Sempre em caixa alta) tem que ter final no inicio.
+
+
+
         System.out.println("Hello, World!");
 
         System.out.println("teste");
